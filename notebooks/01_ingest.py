568da2bc733efd2fcae800f20020b729bf355389
rafaelvp-db <rafaelvp-db>
2023-01-19 10:37:11 +0000

    url

--- notebooks/01_ingest.py
+++ notebooks/01_ingest.py
@@ -5,6 +5,15 @@ output_path = "/dbfs/tmp/covid.csv"
 
 # COMMAND ----------
 
+import requests
+
+url = 'https://covid.ourworldindata.org/data/owid-covid-data.csv'
+output_path = '/dbfs/tmp/covid.csv'
+
+response = requests.get(url)
+with open(output_path, "w") as file:
+  file.write(response.text)
+  
 df = spark.read.csv(output_path.replace("/dbfs", ""), header = True, inferSchema = True)
 display(df)
 
@@ -21,4 +30,8 @@ display(df_europe)
 # COMMAND ----------
 
 spark.sql("create database if not exists covid_fc")
-df_europe.write.saveAsTable("covid_fc.bronze")
+df_europe.write.saveAsTable("covid_fc.bronze", mode = "overwrite")
+
+# COMMAND ----------
+
+
