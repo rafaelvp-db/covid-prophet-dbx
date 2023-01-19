840491246bcd56d73c95cb5ffed4dd9cd86edc2e
Rafael Pierre <rafael.pierre@databricks.com>
2023-01-19 11:04:13 +0100

    pass

---
+++ Makefile
@@ -0,0 +1,2 @@
+integration:
+	dbx execute covid-prophet-dbx-sample-tests --cluster-id 0812-165614-tibia842
---
+++ setup.py
@@ -0,0 +1,46 @@
+"""
+This file configures the Python package with entrypoints used for future runs on Databricks.
+
+Please follow the `entry_points` documentation for more details on how to configure the entrypoint:
+* https://setuptools.pypa.io/en/latest/userguide/entry_point.html
+"""
+
+from setuptools import find_packages, setup
+from covid_prophet_dbx import __version__
+
+PACKAGE_REQUIREMENTS = ["pyyaml", "pytest"]
+
+# packages for local development and unit testing
+# please note that these packages are already available in DBR, there is no need to install them on DBR.
+LOCAL_REQUIREMENTS = [
+    "pyspark==3.2.1",
+    "delta-spark==1.1.0",
+    "scikit-learn",
+    "pandas",
+    "mlflow",
+    "pytest"
+]
+
+TEST_REQUIREMENTS = [
+    # development & testing tools
+    "pytest",
+    "coverage[toml]",
+    "pytest-cov",
+    "dbx>=0.7,<0.8"
+]
+
+setup(
+    name="covid_prophet_dbx",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    setup_requires=["setuptools", "wheel", "pytest"],
+    install_requires=PACKAGE_REQUIREMENTS,
+    extras_require={"local": LOCAL_REQUIREMENTS, "test": TEST_REQUIREMENTS},
+    entry_points = {
+        "console_scripts": [
+            "etl = covid_prophet_dbx.tasks.sample_etl_task:entrypoint",
+            "ml = covid_prophet_dbx.tasks.sample_ml_task:entrypoint",
+    ]},
+    version=__version__,
+    description="",
+    author="",
+)
